0aa136868a4d37bf155c8aea2f19ec00d9288297
jaggehns <jaggehns@gmail.com>
2022-01-16 13:32:21 +0800

    Implemented  pagination component

--- src/components/AnimeTable.tsx
+++ src/components/AnimeTable.tsx
@@ -4,6 +4,7 @@ import { LinearProgress, makeStyles, TextField } from '@material-ui/core'
 import { AnimeList, AnimeSearch } from '../config/api'
 import { IAnimeList } from '../interfaces/IAnimeList'
 import AnimeCard from './AnimeCard'
+import { Pagination } from '@material-ui/lab'
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 const useStyles = makeStyles(theme => ({
@@ -25,20 +26,34 @@ const useStyles = makeStyles(theme => ({
       textAlign: 'center',
     },
   },
+  pagination: {
+    '& .MuiPaginationItem-root': {
+      color: 'gold',
+      '&:hover': {
+        backgroundColor: '#772e2e',
+      },
+
+      '&:focus': {
+        backgroundColor: '#772e2e',
+      },
+    },
+  },
 }))
 
 const AnimeTable: FC = () => {
   const [animeList, setAnimeList] = useState<IAnimeList[]>([])
   const [loading, setLoading] = useState<boolean>(false)
   const [search, setSearch] = useState<string>('')
+  const [page, setPage] = useState<number>(1)
 
   const classes = useStyles()
 
   const searchAnimes = async () => {
     setLoading(true)
     const searchQuery = search.toLowerCase()
+    const searchPage = page
     await axios
-      .get(AnimeSearch(searchQuery))
+      .get(AnimeSearch(searchQuery, searchPage))
       .then(animeList => {
         setAnimeList(animeList.data.results)
         console.log(animeList.data.results)
@@ -49,10 +64,20 @@ const AnimeTable: FC = () => {
       })
   }
 
+  const handleChange = (page: number): void => {
+    setPage(page)
+    window.scroll(0, 0)
+  }
+
   const fetchAnimes = async () => {
     setLoading(true)
+    if (!search || search.length === 0) {
+      setPage(1)
+    }
+    const currentPage = page
+
     await axios
-      .get(AnimeList())
+      .get(AnimeList(currentPage))
       .then(animeList => {
         setAnimeList(animeList.data.top)
         console.log(animeList.data.top)
@@ -64,8 +89,11 @@ const AnimeTable: FC = () => {
   }
 
   useEffect(() => {
-    fetchAnimes()
-  }, [])
+    if (!search || search.length === 0) {
+      setPage(1)
+      fetchAnimes()
+    }
+  }, [page])
 
   useEffect(() => {
     let isApiSubscribed = true
@@ -73,14 +101,12 @@ const AnimeTable: FC = () => {
       searchAnimes()
     } else if (search?.length === 0) {
       fetchAnimes()
-    } else {
-      console.log('Error')
     }
 
     return () => {
       isApiSubscribed = false
     }
-  }, [search])
+  }, [search, page])
 
   // const handleChange = e => {
   //   setSearch(e)
@@ -92,7 +118,7 @@ const AnimeTable: FC = () => {
         <TextField
           className={classes.root}
           variant='filled'
-          placeholder='Search For an Anime...'
+          placeholder='Search For an Anime... (min 3 char)'
           style={{ width: '100%' }}
           InputProps={{ classes: { input: classes.input } }}
           onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearch(e.target.value)}
@@ -107,6 +133,18 @@ const AnimeTable: FC = () => {
           })}
         </div>
       )}
+      <Pagination
+        variant='outlined'
+        onChange={(e: any) => handleChange(e.target.textContent)}
+        style={{
+          padding: 20,
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+        classes={{ ul: classes.pagination }}
+        count={80}
+      />
     </div>
   )
 }
--- src/config/api.ts
+++ src/config/api.ts
@@ -9,6 +9,9 @@ export const AnimeList = (page?: number): string => {
   return `https://api.jikan.moe/v3/top/anime/${page}/tv`
 }
 
-export const AnimeSearch = (searchQuery: string): string => {
-  return `https://api.jikan.moe/v3/search/anime?q=${searchQuery}&page=1`
+export const AnimeSearch = (searchQuery: string, page?: number): string => {
+  if (!page) {
+    page = 1
+  }
+  return `https://api.jikan.moe/v3/search/anime?q=${searchQuery}&page=${page}`
 }
